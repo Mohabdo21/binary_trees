35768075c8d9a1b6102a0297ccfd0d09af8af459
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-01-30 23:37:02 +0200

    dont change the is_AVL i already fixed it xD also reply to discord pls

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -19,8 +19,7 @@ int bst_inorder(const binary_tree_t *tree, const binary_tree_t *prev)
 		if (prev && tree->n <= prev->n)
 			return (0);
 		/* recursive call for right subtree */
-		return (bst_inorder(tree->right, tree) &&
-				bst_inorder(tree->left, tree));
+		return (bst_inorder(tree->right, tree));
 	}
 	return (1);
 }
